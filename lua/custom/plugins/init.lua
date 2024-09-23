c89ee3778595bafd0395726ef503a025023a14a9
Weetile <weetile@hotmail.com>
2024-09-23 12:07:26 +0100

    added nvim-tree and supermaven-nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,22 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'nvim-tree/nvim-tree.lua',
+    config = function()
+      require('nvim-tree').setup {}
+
+      -- Set custom keybinding for <leader>e to toggle nvim-tree
+      vim.keymap.set('n', '<leader>e', function()
+        require('nvim-tree.api').tree.toggle()
+      end, { noremap = true, silent = true, desc = 'Toggle Nvim Tree' })
+    end,
+  },
+  {
+    'supermaven-inc/supermaven-nvim',
+    config = function()
+      require('supermaven-nvim').setup {}
+    end,
+  },
+}
